ba7a7d535855318354de40c1a27c340432043a65
LEO <asda2753596@gmail.com>
2019-04-01 17:21:08 +0800

    add word_count

---
+++ r3.py
@@ -0,0 +1,35 @@
+
+data = []
+count = 0
+with open ('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 10000 == 0:
+			print(len(data))
+
+wc = {}
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+
+while True:
+	word = input('What word do you want to check? ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過得次數為: ', wc[word])
+	else:
+		print('這個是沒有出現過喔')
+
+print('感謝使用此功能')
